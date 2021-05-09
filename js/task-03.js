09c67537a1e09a2a2acce449bb0b6c42ad310882
Stanislav Kruhliak <stanislav.kruhliak@gmail.com>
2021-05-09 23:47:52 +0200

    fix task 3

--- js/task-03.js
+++ js/task-03.js
@@ -9,7 +9,7 @@ const makeTransaction = (transaction) => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       } else {
         reject(transaction.id);
       }
@@ -17,7 +17,7 @@ const makeTransaction = (transaction) => {
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
